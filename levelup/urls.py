f1115a62358fef01ab18117504a7630bb1f72522
hrlnsnchz <hrlnsnchz@gmail.com>
2021-05-27 20:37:12 -0500

    Renders an HTML report of a user's list of games directly from the server.

--- levelup/urls.py
+++ levelup/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include('levelupreports.urls'))
 ]
 
